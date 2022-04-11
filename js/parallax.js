df87e203ff47f64d5eccbd74c43d20b62f2de42e
vonrimS <mr.mytab@gmail.com>
2022-04-11 17:45:38 +0200

    5 commit

--- js/parallax.js
+++ js/parallax.js
@@ -1,84 +1,31 @@
 var home_splitter = document.getElementById('home_splitter');
 var about_splitter = document.getElementById('about_splitter');
-
 var about_container = document.getElementById('about_container');
 var mottoAllLines = document.getElementById('home_motto');
-
 const mottoList = document.querySelectorAll('#home_motto li');
-
 
 window.addEventListener('scroll', function (e) {
   let mottoOffset = mottoAllLines.offsetTop - window.scrollY;
-  console.log(mottoOffset);
-  
+  console.log(mottoOffset);  
 
-  if (mottoOffset < 150) {
-    // console.log(mottoList);
-    mottoList.forEach((line, index) => {
-      // line.style.animation = '';
-      // line.style.animation = `mottoFade 2.5s ease forwards ${index / 2 + 0.2}s`;
-      if (line.style.animation) {
-        line.style.animationDirection = 'initial';
-        // line.style.animation = `mottoFade 0.5s ease forwards ${index/2 + 0.2}s`;
-      } else {
-        // line.style.animation = '';
-        line.style.animation = `mottoFade 2.5s ease forwards ${index / 2 + 0.2}s`;
-        // console.log(line.style.animation.name)
-        // line.style.animation = `mottoFade`;
-        // console.log(index/5 + 0.2);
+  if (mottoOffset < 150) {   
+    mottoList.forEach((line, index) => {     
+      if (line.style.animationDirection === 'reverse') {
+        line.style.animationDirection = 'normal';
+        console.log(line.style.animationDirection);       
+      } else {       
+        line.style.animation = `mottoFade 2s ease forwards ${index / 2 + 0.2}s`;       
       }
     });
   }
 
   if(mottoOffset > 150) {
     mottoList.forEach((line, index) => {
-      if (line.style.animation) {
-
-        line.style.animationDirection = "reverse";
-        
-        line.style.animation = '';
-
-        // line.style.animation = `mottoFade 2.5s ease forwards ${index / 2 + 0.2}s`;
-        // line.style.animation.;
-        // line.style.animation = `mottoFade 0.5s ease forwards ${index/2 + 0.2}s`;
-      } else {
-        // console.log(line.style.animation.name)
-        // line.style.animation = `mottoFade`;
-        // console.log(index/5 + 0.2);
+      if (line.style.animationDirection === 'normal') {
+        line.style.animationDirection = "reverse";        
+        line.style.animation = '';       
+      } else {      
       }
     });
-  }
-  /*
-  if (mottoOffset > 150) {
-    mottoList.forEach((line, index) => {
-      if (line.style.animation.name='mottoFade') {
-        // line.style.animation = '';
-        line.style.animation = `mottoReveal 1s ease-out forwards ${index / 2 + 0.2}s`;       
-           
-      } else {   
-        console.log('...> 150')   
-      }
-    });
-    
-  }*/
-
-    // mottoAllLines.style.opacity = 0;
-    // mottoAllLines.style.transitionDuration = "1000ms";
-  /*} else if (mottoOffset > 150) {
-    // console.log(mottoList);
-    mottoList.forEach((line, index) => {
-      if (line.style.animation) {
-        // line.style.animation = '';
-        // line.style.animation = `mottoFade 0.5s ease forwards ${index/2 + 0.2}s`;
-      } else {
-        // line.style.animation = `mottoFade`;
-        line.style.animation = `mottoFade 0.5s ease forwards ${
-          index / 2 + 0.2
-        }s`;
-        // console.log(index/5 + 0.2);
-      }
-    });
-    // mottoAllLines.style.opacity = 1;
-    // mottoAllLines.style.transitionDuration = "1000ms";
-  }*/
+  }  
 });
