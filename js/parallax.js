0d9bbc1198d27cd71d5d66c485b0710d7e5eb929
vonrimS <mr.mytab@gmail.com>
2022-04-12 12:02:32 +0200

    7 commit

--- js/parallax.js
+++ js/parallax.js
@@ -14,7 +14,7 @@ const x = mottoAllLines.offsetTop;
 window.addEventListener('scroll', function (e) {
   let mottoOffset = mottoAllLines.offsetTop - window.scrollY;
   console.log(mottoOffset);
-  // console.log('const: ' + x)
+  
 
   if (mottoOffset < x) {
     console.log('less than marker')
@@ -24,68 +24,19 @@ window.addEventListener('scroll', function (e) {
         console.log(line.style.animationDirection);
       } else {
         line.style.animation = `mottoFade 2s ease forwards ${index / 2 + 0.2}s`;
+        // console.log(line.style.animation)
       }
     });
   }
 
   if(mottoOffset > 150) {
         mottoList.forEach((line, index) => {
-          if (line.style.animationDirection === 'normal') {
+          if (line.style.animation) {
             // line.style.animationDirection = "reverse";
             line.style.animation = '';
+            // console.log(line.style.animation)
           } else {
           }
         });
       }
 });
-/*
-const faders = document.querySelectorAll('.fade-in');
-console.log(faders);
-
-const options = {
-  // root: null,
-  root: document.querySelector('#home'),
-  rootMargin: '0px',
-  threshold: 1.0,
-};
-
-const appearOnScroll = new InteractionObserver(function (
-  entries,
-  appearOnScroll
-) {
-  entries.forEach((entry) => {
-    // if (!entry.isIntersecting) {
-    //   console.log(entry);
-    //   return;
-    // } else {
-      entry.target.classList.add('appear');
-      console.log(entry);
-      // appearOnScroll.unobserve(entry.target);
-    // }
-  });
-},
-options);
-
-faders.forEach((fader) => {
-  appearOnScroll.observe(fader);
-});*/
-
-// var target = document.getElementById('home');
-
-// let options = {
-//   root: null,
-//   rootMargin: '0px',
-//   threshold: 1.0
-// }
-
-// let callback = (entries, observer) => {
-//   entries.forEach(entry => {
-//     if (entry.target.id === 'target'){
-//       target.innerText = (entry.isIntersecting) ? '11111' : '00000'
-//     }
-//   })
-
-// }
-
-// let observer = new InteractionObserver(callback, option);
-// observer.observe(target);
