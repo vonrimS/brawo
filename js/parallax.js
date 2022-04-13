e544097c978df87f35c49010e10436e2382b80cf
vonrimS <mr.mytab@gmail.com>
2022-04-13 16:19:53 +0200

    8 commit

--- js/parallax.js
+++ js/parallax.js
@@ -3,10 +3,7 @@ var about_splitter = document.getElementById('about_splitter');
 var about_container = document.getElementById('about_container');
 var mottoAllLines = document.getElementById('home_motto');
 const mottoList = document.querySelectorAll('#home_motto li');
-// const mottoList = document.getElementsById('#home_motto');
 
-// console.log(mottoList)
-// const mottoList = document.querySelectorAll('.fade-in');
 
 console.log(mottoList);
 const x = mottoAllLines.offsetTop;
@@ -23,18 +20,15 @@ window.addEventListener('scroll', function (e) {
         line.style.animationDirection = 'normal';
         console.log(line.style.animationDirection);
       } else {
-        line.style.animation = `mottoFade 2s ease forwards ${index / 2 + 0.2}s`;
-        // console.log(line.style.animation)
+        line.style.animation = `mottoFade 2s ease forwards ${index / 2 + 0.2}s`;        
       }
     });
   }
 
   if(mottoOffset > 150) {
         mottoList.forEach((line, index) => {
-          if (line.style.animation) {
-            // line.style.animationDirection = "reverse";
-            line.style.animation = '';
-            // console.log(line.style.animation)
+          if (line.style.animation) {            
+            line.style.animation = '';           
           } else {
           }
         });
